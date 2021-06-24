e9735023b09b80be9ea81135ccf9f55c84357346
allansrc <allanramos.teixeira@gmail.com>
2021-06-24 12:33:52 -0300

    update example dependences

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -4,9 +4,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <responsive_table_plugin.h>
+#include <flutter_responsive_table/responsive_table_plugin.h>
 
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ResponsiveTablePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ResponsiveTablePlugin"));
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_responsive_table_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ResponsiveTablePlugin");
+  responsive_table_plugin_register_with_registrar(flutter_responsive_table_registrar);
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import responsive_table
+import flutter_responsive_table
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ResponsiveTablePlugin.register(with: registry.registrar(forPlugin: "ResponsiveTablePlugin"))
